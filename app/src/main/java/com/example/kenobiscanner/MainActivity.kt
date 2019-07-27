145c19a9064f396d1784ac0e40459a55f4484e4c
unknown <scekicogi@gmail.com>
2019-07-27 21:10:22 +0200

    Created starting files
    Created MainActivity and its layout and added click listener on
    scan button that leads to another activity.

--- app/src/main/java/com/example/kenobiscanner/MainActivity.kt
+++ app/src/main/java/com/example/kenobiscanner/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.kenobiscanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Main Activity that is started when app starts
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scan_button.setOnClickListener { openScanActivity() }
+    }
+
+    private fun openScanActivity() {
+        val scanActivityIntent = Intent(this, ScanActivity::class.java)
+        startActivity(scanActivityIntent)
     }
 }
---
+++ app/src/main/java/com/example/kenobiscanner/ScanActivity.kt
@@ -0,0 +1,8 @@
+package com.example.kenobiscanner
+
+/**
+ * Scan Activity where all the scanning work is done
+ */
+class ScanActivity {
+
+}
